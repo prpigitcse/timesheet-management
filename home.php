21e2728d9e79b3c06bbb3d07abe555fedf03f66a
SureshPrabhuParkala <sureshprabhuparkala@gmail.com>
2019-12-04 19:01:38 +0530

    done with the corrections

--- home.php
+++ home.php
@@ -5,18 +5,19 @@
         header("Location: login.php");
     else{
 ?>
-    <!DOCTYPE html>
-        <head>
-            <title>User Home</title>
-            <!-- <link rel="stylesheet" type="text/css" href="csvtable.css"> -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        </head>
-        <?php
-            $uid=$_SESSION['uid'];
-            $result = mysqli_query($conn, "SELECT * FROM files where uid='$uid'");
-        ?>
-        <body>
-            <?php require_once("header.php");?>
+<!DOCTYPE html>
+    <head>
+        <title>User Home</title>
+        <!-- <link rel="stylesheet" type="text/css" href="csvtable.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    <?php
+        $uid=$_SESSION['uid'];
+        $result = $conn -> query("SELECT * FROM files where uid='$uid'");
+    ?>
+    <body>
+        <?php require_once("header.php");?>
+        <div class="card">
             <table class="table table-striped table-bordered table-reponsive offset-md-2 col-md-8 mt-5">
                 <thead>
                     <tr>
@@ -25,7 +26,7 @@
                         <td>Status</td>
                     </tr>
                 </thead>
-                <?php while($row = mysqli_fetch_assoc($result)) { ?>
+                <?php while($row = $result -> fetch_assoc()) { ?>
                     <tr>
                         <td><?php echo $row['fileid'] ?></td>
                         <td><?php echo $row['path'] ?></td>
@@ -33,6 +34,7 @@
                     </tr>
                 <?php } ?>
             </table>
-        </body>
-    </html>
+        </div>
+    </body>
+</html>
 <?php } ?>
--- login.php
+++ login.php
@@ -6,6 +6,9 @@ include_once("dbConnect.php");
 <!doctype html>
     <head>
         <title>Login</title>
+        <script language="javascript" type="text/javascript">
+            window.history.forward();
+        </script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel='stylesheet' href='login.css'>
     </head>
@@ -13,7 +16,7 @@ include_once("dbConnect.php");
         <section class='container col-8 col-sm-6 col-md-6 col-lg-3'>
             <section class="row justify-content-center">
                 <div>
-                    <form action="" method='post'>
+                    <form action="loginaction.php" method='post'>
                         <div class="form-group">
                             <h1>Login</h1>
                             <label>Email:</label>
@@ -23,46 +26,17 @@ include_once("dbConnect.php");
                             <label>Password:</label>
                             <input type="password" class="form-control" placeholder="Enter password" name="pass">
                         </div>
+                        <?php
+                            if(isset($_GET['error'])==TRUE){
+                                echo "<p style='color:red;text-align:center'>".$_GET['error']."</p>";
+                            }
+                        ?>
                         <button type="submit" class="btn btn-primary" name="login">Login</button>
                         <a href="registrationform.php">New User? Register</a>
                     </form>
                 </div>
             </section>
         </section>
-        <?php
-            if(isset($_POST["login"])){
-                if(!empty($_POST['email']) && !empty($_POST['pass'])){
-                    $email = $_POST['email'];
-                    $pass = $_POST['pass'];
-                    $result = mysqli_query($conn, "SELECT * FROM registration WHERE email='".$email."'");
-                    $num = mysqli_num_rows($result);
-                    if($num!=0){
-                        while($row = mysqli_fetch_assoc($result)){
-                            $dbemail = $row['email'];
-                            $dbpass = $row['password'];
-                            $fname = $row['fname'];
-                            $lname = $row['lname'];
-                            $uid = $row['uid'];
-                        }
-                        if($email == $dbemail && password_verify($pass,$dbpass)){
-                            if($email == "admin@specbee.com")
-                                header("Location:user-timesheet.php");
-                            else{
-                                $_SESSION['user']=$fname.$lname;
-                                $_SESSION['uid']=$uid;
-                                header("Location:home.php");
-                            }
-                        }
-                        else
-                        echo "<script>alert('Incorrect Email or Password');</script>";
-                    }
-                    else
-                        echo "<script>alert('Invalid Email or Password');</script>";
-                }
-                else
-                    echo "<script>alert('Required all fields!!');</script>";
-            }
-        ?>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
